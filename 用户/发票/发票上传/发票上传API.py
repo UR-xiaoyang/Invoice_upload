425eb309ffea8de4dbd56d1d8686e9d55c226e6c
UR-xiaoyang <2422286329@qq.com>
2024-06-17 22:55:50 +0800

    将API接口改为英文，并将token改为在头部获取

--- 用户/发票/发票上传/发票上传API.py
+++ 用户/发票/发票上传/发票上传API.py
@@ -1,25 +1,23 @@
-from fastapi import APIRouter, UploadFile, Form, File, Request
+from typing import List
+from fastapi import APIRouter, UploadFile, File, Request, Depends
+
+from 数据库.日志.log表 import 记录日志
 from 用户.发票.发票上传.发票上传 import 存储发票
 from 用户.登陆.登陆令牌 import 验证令牌
 
 发票上传路由 = APIRouter()
 
-@发票上传路由.post("/上传")
+@发票上传路由.post("/upload_invoice")
 async def 发票上传API(
     请求: Request,
-    token: str = Form(...),
-    用户名: str = Form(...),  # 根据发票上传表单的其他字段添加更多参数
-    文件上传: UploadFile = File(...)
+    upload_file: List[UploadFile] = File(...)
 ):
-
-    验证结果 = 验证令牌(token)
+    验证结果 = 验证令牌(请求.cookies.get("token"))
     if 验证结果:
         # 获取IP
         IP = 请求.client.host
-        检测上传 = 存储发票(文件上传, 用户名, IP)
-        if 检测上传:
-            return {"code": 200, "msg": "上传成功"}
-        else:
-            return {"code": 400, "msg": "上传失败"}
+        for 文件index in range(len(upload_file)):
+            存储发票(upload_file[文件index], 验证结果['sub'], IP)
+        return {"code": 200, "msg": "上传成功"}
     else:
         return {"code": 400, "msg": "登陆过期"}
