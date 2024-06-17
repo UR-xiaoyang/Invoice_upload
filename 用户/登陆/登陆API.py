3d032858ade76ebc984b19ff9b9e81ea1c2bbe49
UR-xiaoyang <2422286329@qq.com>
2024-06-17 22:53:50 +0800

    将API接口改为英文

--- 用户/登陆/登陆API.py
+++ 用户/登陆/登陆API.py
@@ -7,7 +7,7 @@ from 用户.登陆.登陆表单 import 登陆表单
 
 登陆路由 = APIRouter()
 
-@登陆路由.post("/登陆")
+@登陆路由.post("/sign_in")
 async def 登陆API(请求: Request, 表单: 登陆表单):
     用户登录(表单.用户名, 表单.密码, 请求.client.host)
     if 用户登录(表单.用户名, 表单.密码, 请求.client.host) == "密码错误，请重试。":
@@ -16,4 +16,4 @@ async def 登陆API(请求: Request, 表单: 登陆表单):
         raise HTTPException(status_code=400, detail="用户名或密码错误")
     else:
         生成令牌 = 生成访问令牌(data={"sub": 表单.用户名})
-        return {"access_token": 生成令牌, "token_type": "bearer"}
+        return {"access_token": 生成令牌, "token_type": "bearer", "username": 表单.用户名}
