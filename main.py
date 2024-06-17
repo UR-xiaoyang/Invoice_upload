b526e0d0ab50b0fa397d0b9261cd9b38a37224b4
UR-xiaoyang <2422286329@qq.com>
2024-06-17 22:56:17 +0800

    添加对OPTIONS进行响应

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette import status
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.responses import JSONResponse
 
 from 用户.发票.发票上传.发票上传API import 发票上传路由
 from 用户.发票.发票查询.发票查询API import 发票查询路由器
 from 用户.注册.注册API import 注册路由
 from 用户.登陆.登陆API import 登陆路由
 
-# 导入其他 API 路由...
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 包含 API 路由
-app.include_router(注册路由, prefix="/用户")
-app.include_router(登陆路由, prefix="/用户")
-app.include_router(发票上传路由, prefix="/用户")
-app.include_router(发票查询路由器, prefix="/用户")
-# 包含其他 API 路由...
+app.include_router(注册路由, prefix="/user")
+app.include_router(登陆路由, prefix="/user")
+app.include_router(发票上传路由, prefix="/user")  # 修改前缀以避免冲突
+app.include_router(发票查询路由器, prefix="/user")  # 修改前缀以避免冲突
 
 # 启动 FastAPI 应用
 if __name__ == "__main__":
