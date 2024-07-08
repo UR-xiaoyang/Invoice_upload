3191563c6ba05817f467ab4ed5a5f6aef38b111b
UR-xiaoyang <2422286329@qq.com>
2024-07-08 22:08:56 +0800

    使用fastapi的HTTPBearer来获取token

--- 用户/发票/发票上传/发票上传API.py
+++ 用户/发票/发票上传/发票上传API.py
@@ -1,5 +1,6 @@
 from typing import List
-from fastapi import APIRouter, UploadFile, File, Request, Depends
+from fastapi import APIRouter, UploadFile, File, Request, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from 数据库.日志.log表 import 记录日志
 from 用户.发票.发票上传.发票上传 import 存储发票
@@ -7,12 +8,16 @@ from 用户.登陆.登陆令牌 import 验证令牌
 
 发票上传路由 = APIRouter()
 
+security = HTTPBearer()
+
 @发票上传路由.post("/upload_invoice")
 async def 发票上传API(
     请求: Request,
-    upload_file: List[UploadFile] = File(...)
+    upload_file: List[UploadFile] = File(...),
+    credentials: HTTPAuthorizationCredentials = Depends(security)
 ):
-    验证结果 = 验证令牌(请求.cookies.get("token"))
+    token = credentials.credentials
+    验证结果 = 验证令牌(token)
     if 验证结果:
         # 获取IP
         IP = 请求.client.host
@@ -20,4 +25,7 @@ async def 发票上传API(
             存储发票(upload_file[文件index], 验证结果['sub'], IP)
         return {"code": 200, "msg": "上传成功"}
     else:
-        return {"code": 400, "msg": "登陆过期"}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="登陆过期"
+        )
