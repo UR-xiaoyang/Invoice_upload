8b5cdcbb48c995b3417ef5c1123ce1527b8c530f
UR-xiaoyang <2422286329@qq.com>
2024-10-21 02:02:50 +0800

    ocr_image改为CPU处理

--- 发票处理/数据识别/OCR/OCR.py
+++ 发票处理/数据识别/OCR/OCR.py
@@ -6,7 +6,7 @@ class OCR_PaddleOCR:
         结果 = []
         # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
         # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
-        ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+        ocr = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=False)
         result = ocr.ocr(img_path, cls=True)
         for idx in range(len(result)):
             res = result[idx]
@@ -17,7 +17,7 @@ class OCR_PaddleOCR:
     def ocr_pdf(pdf_path):
         结果 = []
         PAGE_NUM = 1  # 将识别页码前置作为全局，防止后续打开pdf的参数和前文识别参数不一致 / Set the recognition page number
-        ocr = PaddleOCR(use_angle_cls=True, lang="ch", page_num=PAGE_NUM)  # need to run only once to download and load model into memory
+        ocr = PaddleOCR(use_angle_cls=True, lang="ch", page_num=PAGE_NUM, use_gpu=False)  # need to run only once to download and load model into memory
         #ocr = PaddleOCR(use_angle_cls=True, lang="ch", page_num=PAGE_NUM, use_gpu=0)  # 如果需要使用GPU，请取消此行的注释 并注释上一行 / To Use GPU,uncomment this line and comment the above one.
         result = ocr.ocr(pdf_path, cls=True)
 
